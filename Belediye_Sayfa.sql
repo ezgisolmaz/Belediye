090ccde253a6c281892ef86c55c4040a4e0cd659
ezgisolmaz <ezgisolmaz123@gmail.com>
2013-12-02 13:26:05 +0200

    Belediye_Sayfa

---
+++ Belediye_Sayfa.sql
@@ -0,0 +1,53 @@
+var menu1=new Array()
+menu1[0]='<a href="baskan_baskanlik.php">Baþkanlýk</a>'
+menu1[1]='<a href="baskan_biyografi.php">Biyografi</a>'
+menu1[2]='<a href="baskan_foto.php">Fotoðraflar</a>'
+menu1[3]='<a href="baskan_iletisim.php">Ýletiþim</a>'
+menu1[4]='<a href="baskan_oduller.php">Ödüller</a>'
+
+
+
+var menu2=new Array()
+menu2[0]='<a href="kurumsal_baskanlarimiz.php">Eski Baþkanlarýmýz</a>'
+menu2[1]='<a href="kurumsal_etik_komisyonu.php">Etik Komisyonu</a>'
+menu2[2]='<a href="kurumsal_idari_sema.php">Ýdari Þema</a>'
+menu2[3]='<a href="kurumsal_idari_personel.php">Ýdari Personel Listesi</a>'
+menu2[4]='<a href="kurumsal_ic_kontrol.php">Ýç Kontrol</a>'
+menu2[5]='<a href="kurumsal_komisyon.php">Komisyonlar</a>'
+menu2[6]='<a href="kurumsal_meclis.php">Meclis Üyeleri</a>'
+menu2[7]='<a href="kurumsal_mevzuat.php">Mevzuat</a>'
+menu2[8]='<a href="kurumsal_sirketler.php">Þirketler</a>'
+menu2[9]='<a href="kurumsal_misyon.php">Vizyon & Misyon</a>'
+menu2[10]='<a href="kurumsal_yetki.php">Yetki & Sorumluluklar</a>'
+
+
+var menu3=new Array()
+menu3[0]='<a href="duyurular_list.php">Duyurular</a>'
+menu3[1]='<a href="etkinlikler_list.php">Etkinlikler</a>'
+menu3[2]='<a href="haber_list.php">Haberler</a>'
+menu3[3]='<a href="ihale_list.php">Ýhale Ýlanlarý</a>'
+menu3[4]='<a href="imar_plan_degisiklik.php">Ýmar Planý Ýlanlarý</a>'
+menu3[5]='<a href="guncel_ulusal_basin.php">Ulusal Basýnda</a>'
+
+var menu4=new Array()
+menu4[0]='<a href="https://interactive.eskisehir-bld.gov.tr/" target="_blank">Elektronik Ödeme</a>'
+menu4[1]='<a href="http://interactive.eskisehir-bld.gov.tr:8282/Default.aspx" target="_blank">e-Dilekçe</a>'
+
+var menu5=new Array()
+menu5[0]='<a href="bilgi_edinme.php">Bilgi Edinme Formu</a>'
+menu5[1]='<a href="bilem.php">Dilek & Þikayetler</a>'
+menu5[2]='<a href="bilgi_edinme_faaliyet_rapor.php">Faaliyet Raporlarý</a>'
+menu5[3]='<a href="bilgi_edinme_kamu_hiz.php">Kamu Hizmet Standartý</a>'
+menu5[4]='<a href="bilgi_edinme_meclis_karar.php">Meclis Bilgi Sistemi</a>'
+menu5[5]='<a href="bilgi_edinme_performans_prog.php">Performans Programý</a>'
+
+var menu6=new Array()
+menu6[0]='<a href="bizim_sehir.php">Bizim Þehir</a>'
+menu6[1]='<a href="esbot_saatleri.php">Esbot Seferleri</a>'
+menu6[2]='<a href="eskisehir_hakkinda.php">Eskiþehir Hakkýnda</a>'
+menu6[3]='<a href="hal_fiyatlari.php">Hal Fiyatlarý</a>'
+menu6[4]='<a href="http://www.eeo.org.tr/nobetci-eczaneler/" target="_blank">Nöbetçi Eczaneler</a>'
+menu6[5]='<a href="otobus_saatleri.php">Otobüs Saatleri</a>'
+menu6[6]='<a href="sss.php">SSS</a>'
+menu6[7]='<a href="http://www.estram.com.tr/tramvay_estram.php" target="_blank">Tramvay Saatleri</a>'
+menu6[8]='<a href="tarifeler.php">Tarifeler</a>'
